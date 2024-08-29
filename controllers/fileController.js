e3f720b8e384dfa03c5a8781d019c2e0fd319be7
wsoham22 <139250905+wsoham22@users.noreply.github.com>
2024-08-29 18:40:08 +0530

    commit 02

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,38 @@
 const User = require('../models/user');
 const upload = require('../config/multerConfig');
+const fs = require('fs');
+const path = require('path');
+exports.deleteResume = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
 
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if (user.resume && user.resume.path) {
+            // Remove the file from the server
+            const filePath = path.join(__dirname, '..', user.resume.path);
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error('File deletion error:', err);
+                }
+            });
+
+            // Remove the resume information from the user
+            user.resume = {};
+            await user.save();
+
+            res.json({ message: 'Resume deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'No resume to delete' });
+        }
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
 exports.uploadResume = (req, res) => {
     upload.single('resume')(req, res, async (err) => {
         if (err) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { protect } = require('../middlewares/authmiddleware');
 const { getUserProfile, updateUserProfile } = require('../controllers/userController');
-const { uploadResume } = require('../controllers/fileController');
+const { uploadResume,deleteResume } = require('../controllers/fileController');
 const router = express.Router();
 
 router.route('/profile').get(protect, getUserProfile).patch(protect, updateUserProfile);
 router.post('/upload', protect, uploadResume);
+router.delete('/deleteresume', protect, deleteResume);
 module.exports = router;
